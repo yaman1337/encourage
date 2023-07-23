aa6c54df4960dcdd7a9965d74d295afe5266ac61
yaman1337 <yaman.sarabariya@gmail.com>
2023-07-23 23:24:40 +0545

    add key for map

--- src/components/landing_page/Gallery.tsx
+++ src/components/landing_page/Gallery.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 type GalleryCardProp = {
   image: string;
   title: string;
+  key?: string | number
 };
 
-const GalleryCard = ({ image, title }: GalleryCardProp) => {
+const GalleryCard = ({ image, title, key }: GalleryCardProp) => {
   return (
-    <div className=" shadow-lg w-full h-[250px]  border rounded-md cursor-pointer  hover:shadow-2xl">
+    <div  key={key} className=" shadow-lg w-full h-[250px]  border rounded-md cursor-pointer  hover:shadow-2xl">
       <Image
         alt="image"
         src={EventImage}
@@ -36,7 +37,7 @@ export default function Gallery() {
 
       <div className="grid gap-4 my-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:px-10">
         {data.map((item, key) => (
-          <GalleryCard image={item.image} title={item.title} />
+          <GalleryCard image={item.image} title={item.title} key={key} />
         ))}
       </div>
     </div>
